587cfa5a51f0f8b9dbe184bdc4d65d047b25eaed
Obada Jabr <s11941370@stu.najah.edu>
2026-01-18 14:34:56 +0200

    dark theme

--- js/script.js
+++ js/script.js
@@ -20,3 +20,37 @@ function generatePass() {
    }
 }
 
+function copyToClipboardOne() {
+    outputOne.select();
+    outputOne.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(outputOne.value);
+     if(!outputOne.value ==""){
+     text = document.querySelector('.text');
+    text.innerText = "Password Copied!";
+    }
+
+}
+function copyToClipboardTwo(){
+    outputTwo.select();
+    outputTwo.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(outputTwo.value);
+    if(!outputTwo.value ==""){
+     text = document.querySelector('.text');
+    text.innerText = "Password Copied!";
+    }
+    
+}
+
+let darkMode = document.querySelector('.dark');
+let container = document.querySelector('.container');
+let svg = document.getElementById('svg');
+
+function darkmodeToggle() {
+    container.classList.toggle('dark-mode');
+    if(container.classList.contains('dark-mode')){
+        svg.style.fill = "#000000";
+    } else {
+        svg.style.fill = "#ffffff";
+    }       
+    svg.classList.toggle('dark-svg');
+}
